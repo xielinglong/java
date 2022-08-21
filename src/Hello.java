5378de1bc09c5003cfc4c6683e973efc1ccc6625
xieninglong <952898287@qq.com>
2022-08-21 17:15:11 +0800

    add Hello.java

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+class Hello {
+    public void hello() {
+        System.out.println("Hello World!");
+    }
+    public void hello(String name) {
+        System.out.println("Hello " + name + "!");
+    }
+    public void hello(String name, int age) {
+        if (age < 18) {
+            System.out.println("Hi, " +name + "!");
+        }else {
+            System.out.println("Hello, " + name + "!");
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,26 @@ public class Main {
         Person ming = new Person();
         ming.setBirth(1996);
         System.out.println(ming.getAge());
+        Person ning = new Person("小宁", 26);
+        System.out.println(ning.getName1());
+        System.out.println(ning.getAge1());
+        Student xieninglong = new Student();
+        xieninglong.setName("谢宁龙");
+        xieninglong.setScore(100);
+        System.out.println(xieninglong.getName() + "的分数：" + xieninglong.getScore());
     }
 }
 
 class Person {
     private String name;
     private int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public Person() {
+
+    }
     private int birth;
     public void setBirth(int birth) {
         this.birth = birth;
@@ -41,5 +55,20 @@ class Person {
             throw new IllegalArgumentException("invalid age value");
         }
         this.age = age;
+    }
+    public String getName1() {
+        return name;
+    }
+    public int getAge1() {
+        return age;
+    }
+}
+class Student extends Person {
+    private int score;
+    public int getScore() {
+        return this.score;
+    }
+    public void setScore(int score) {
+        this.score = score;
     }
 }
